6b99d1e5eeefc1cd42f2a71f70b4348f7af2953c
Tunc <tuncgonelfb@hotmail.com>
2021-12-23 22:47:49 -0500

    difference function finished

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ for i in selected_races:
             temp = ff1.get_session(2021, i, l).load_laps()
             temp_time = temp.pick_driver(drivers[k]).pick_fastest()
             test = str(temp_time['LapTime'])
-            final_temp = test[7:]
+            final_temp = test[10:]
             last_lap_times = temp_driver.get(drivers[k])
             last_lap_times[l] =  final_temp
             temp_driver[drivers[k]] = last_lap_times
@@ -50,12 +50,40 @@ print(final_data)
 lec_difference = []
 sai_difference = []
 
+def StrToInt(time) -> float:
+
+    temp_time = time.split(':')
+    num = temp_time[0] + temp_time[1]
+    num = float(num)
+    return num
+
 def Compare_Times(data):
 
     for i in selected_races:
+        temp_race = data.get(i)
 
+        for l in selected_sessions:
+            temp_driver1 = temp_race.get('SAI')
+            temp_driver2 = temp_race.get('LEC')
+
+            time_lec = temp_driver2.get(l)
+            time_sai = temp_driver1.get(l)
+
+            time_sai = StrToInt(time_sai)
+            time_lec = StrToInt(time_lec)
+
+            if time_lec > time_sai:
+
+                num = time_lec - time_sai
+                sai_difference.append(num)
+
+            if time_sai > time_lec:
+
+                num = time_sai - time_lec
+                lec_difference.append(num)
 
         pass
+
 
 
 Compare_Times(final_data)
